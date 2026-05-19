fc839f61ec131246b39ded46d0e8ac423d014ea8
alon1man <thebrickseparatorsfll@gmail.com>
2026-05-19 20:20:36 +0300

    Update vigenere.py

--- encryption/vigenere.py
+++ encryption/vigenere.py
@@ -6,7 +6,7 @@ def encrypt_vigenere(text, key):
 
     for char in text:
         if char.lower() in alpha:
-            # Shift = position of text char + position of key char
+          
             placement = alpha.find(char.lower())
             shiftlike_ofchar = alpha.find(key[ki % len(key)])
             new_char = alpha[(placement + shiftlike_ofchar) % 26]
@@ -28,12 +28,11 @@ def vigenere_decrypt(text, key):
             placement = alpha.find(char.lower())
             shiftlike_ofchar = alpha.find(key[ki % len(key)])
 
-            # כל השורות האלו חייבות להיות בתוך ה-if
+        
             new_char = alpha[(placement - shiftlike_ofchar) % 26]
             result += new_char.upper() if char.isupper() else new_char
             ki += 1
         else:
-            # כאן מטפלים בתווים שהם לא אותיות (כמו רווחים)
             result += char
             
     return result
